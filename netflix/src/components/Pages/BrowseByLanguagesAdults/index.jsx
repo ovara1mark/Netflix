4d33eb95e94710e0e35f38fa90a80eda7e0a4441
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-11-05 08:48:02 +0100

    File updates

--- netflix/src/components/Pages/BrowseByLanguagesAdults/index.jsx
+++ netflix/src/components/Pages/BrowseByLanguagesAdults/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const BrowseByLahnguagesAdults = () => {
+export const BrowseByLanguagesAdults = () => {
     return (
-        <div><Hello></Hello></div>
+        <div>Hello</div>
     )
-}
+};
