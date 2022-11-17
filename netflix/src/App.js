1f95ce4467f40354671b0b6fde7ff254f2b31843
Ese2020 <89306416+Ese2020@users.noreply.github.com>
2022-11-17 14:34:40 +0100

    update

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,42 +1,10 @@
 import "./index.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import {
-  BrowseByLanguagesAdults,
-  SignIn,
-  HomeAdults,
-  LandingPage,
-  MoviesAdults,
-  MyListAdults,
-  NewAndPopularAdults,
-  TvShowsAdults,
-  Registration,
-  RegistrationDetails,
+import {AdultProfile,
 } from "./components";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/homeadults" element={<HomeAdults />} />
-        <Route exact path="/tvshowsadults" element={<TvShowsAdults />} />
-        <Route exact path="/moviesadults" element={<MoviesAdults />} />
-        <Route
-          exact
-          path="/newandpopularadults"
-          element={<NewAndPopularAdults />}
-        />
-        <Route exact path="/mylistadults" element={<MyListAdults />} />
-        <Route
-          exact
-          path="browsebylanguages"
-          element={<BrowseByLanguagesAdults />}
-        />
-        <Route exact path="signin" element={<SignIn />} />
-        <Route exact path="registration" element={<Registration />} />
-        <Route exact path="/signup/regform" element={<RegistrationDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <AdultProfile />
   );
 }
 
